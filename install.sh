74578a0ceb192e7c3ede93dff2709588b897d7df
Victor Rachieru <vick91r@gmail.com>
2015-08-17 16:19:08 +0300

    Top-level install script

--- install.sh
+++ install.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+cd "$(dirname "$BASH_SOURCE")" \
+  && source 'os/utils.sh'
+
+main() {
+  ask_for_sudo
+
+  install_step 'Create directories' \
+    'Do you want the additional directories to be created?' \
+    ['create_directories']
+
+  install_step 'Create symbolic links' \
+    ['create_symbolic_links']
+
+  install_step 'Install applications' \
+    'Do you want to install the applications/command line tools?' \
+    ['install_applications']
+
+  install_step 'Set preferences' \
+    'Do you want to set the custom preferences?' \
+    ['set_preferences']
+
+  install_step 'Restart' \
+    'Do you want to restart?' \
+    ['restart']
+}
+
+main
